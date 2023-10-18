1b55c10ab99c75149233292ecec17f8b56bad07d
4E6F626F64790A <4E6F626F64790A@protonmail.com>
2023-10-18 02:03:50 -0300

    First commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,272 @@
+"use client";
+import React, { useEffect } from 'react';
+import Navbar from '@/components/Navbar'
+import Carousel from '@/components/Carrosel';
 import Image from 'next/image'
+import "../styles/main.css"
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <>
+    <Navbar />
+    <main>
+      <section id="home">
+      <h1>Aqui na Krystal Festas seus sonhos brilham!</h1>
+        <p>
+          ​À mais de 17 anos no ramo de festas e eventos, uma das melhores
+          casas de festas de Vista Alegre.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p>
+          Salão refrigerado com capacidade para 250 pessoas e salão de jogos, ampla pista de dança, acesso para cadeirantes.
+        </p>
+        <p>
+          Consulte nossos pacotes de casamento, bodas, 15 anos, temáticos,
+          infantis e se encante com o capricho, a beleza a qualidade e os
+          preços imbatíveis.
+        </p>
+        <button class="home-button">AGENDAR UMA VISITA</button>
+      </section>
+
+      <section id="services">
+        <h1>Serviços</h1>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Recursos</th>
+              </tr>
+            </thead>
+            <tbody>
+  <tr>
+    <td>Duração</td>
+  </tr>
+  <tr>
+    <td>Salão Climatizado</td>
+  </tr>
+  <tr>
+    <td>Decoração do Salão</td>
+  </tr>
+  <tr>
+    <td>Mesa do Bolo</td>
+  </tr>
+  <tr>
+    <td>Mini Jantar</td>
+  </tr>
+  <tr>
+    <td>Som (DJ + Pista de Dança)</td>
+  </tr>
+  <tr>
+    <td>Sala de Jogos</td>
+  </tr>
+  <tr>
+    <td>Cerveja</td>
+  </tr>
+  <tr>
+    <td>Refrigerantes, Sucos e Água</td>
+  </tr>
+  <tr>
+    <td>Salgados</td>
+  </tr>
+  <tr>
+    <td>Bolo</td>
+  </tr>
+  <tr>
+    <td>Doces</td>
+  </tr>
+  <tr>
+    <td>Equipe</td>
+  </tr>
+  <tr>
+    <td>Álbum de Fotos e Filmagem</td>
+  </tr>
+            </tbody>
+          </table>
+          <table>
+        <thead>
+  <tr>
+    <th>Casamento e 15 Anos</th>
+  </tr>
+        </thead>
+        <tbody>
+  <tr>
+    <td>5 horas</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, 3 arranjos florais mais mesa de frios</td>
+  </tr>
+  <tr>
+    <td>Sim, 2 massas mais 1 caldo</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, Fondados e Caramelados</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Opcional</td>
+  </tr>
+        </tbody>
+          </table>
+          <table>
+        <thead>
+  <tr>
+    <th>Infantil</th>
+  </tr>
+        </thead>
+        <tbody>
+  <tr>
+    <td>4 ou 5 horas</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, com tema da festa mais mesa de guloseimas</td>
+  </tr>
+  <tr>
+    <td>Não</td>
+  </tr>
+  <tr>
+    <td>Sim, com animação, mais brinquedos</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, 3 tipos de lanches</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, Brigadeiros Beijinhos Cajuzinhos</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Não</td>
+  </tr>
+        </tbody>
+          </table>
+          <table>
+        <thead>
+  <tr>
+    <th>Outros Temas*</th>
+  </tr>
+        </thead>
+        <tbody>
+  <tr>
+    <td>4 ou 5 horas</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, mesa de frios ou guloseimas</td>
+  </tr>
+  <tr>
+    <td>Sim, 1 massa</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, ou churrasco com guarnição</td>
+  </tr>
+  <tr>
+    <td>Sim</td>
+  </tr>
+  <tr>
+    <td>Sim, Brigadeiros Beijinhos Cajuzinhos</td>
+  </tr>
+  <tr>
+    <td>SimChurrasqueiro ou Fritadeira</td>
+  </tr>
+  <tr>
+    <td>Não</td>
+  </tr>
+        </tbody>
+          </table>
         </div>
+        <small>*Festa Teen, Temática ou Churrasco, Confraternização de Empresas e Formatura</small>
+      </section>
+
+      <section id="gallery">
+      <h1>Galeria de Fotos</h1>
+      <article>
+      <div className='bo'>
+        <h2>Festa Infantil</h2>
+        <Carousel
+                images={[
+                  <Image width={500} height={500} src="https://static.wixstatic.com/media/59faf3_cab1dcd4ecaf4a6b85897d0df15d0631~mv2.jpg/v1/fill/w_960,h_540,al_c,q_85,enc_auto/59faf3_cab1dcd4ecaf4a6b85897d0df15d0631~mv2.jpg" />,
+                  <Image width={500} height={500} src="https://static.wixstatic.com/media/59faf3_3162c4c46b0b45f78401fe4fa04a7bec~mv2_d_3264_1836_s_2.jpg/v1/fill/w_1417,h_797,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/59faf3_3162c4c46b0b45f78401fe4fa04a7bec~mv2_d_3264_1836_s_2.jpg" />,
+                  <Image width={500} height={500} src="https://static.wixstatic.com/media/59faf3_1be312031bbc4490af267ea13688d629~mv2_d_3264_1836_s_2.jpg/v1/fill/w_1417,h_797,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/59faf3_1be312031bbc4490af267ea13688d629~mv2_d_3264_1836_s_2.jpg" />,
+                ]}
+              />
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='bo'>
+        <h2>Casamentos</h2>
+        <Carousel
+                images={[
+                  <Image width={500} height={500} src="https://static.wixstatic.com/media/59faf3_4e0ce7f6660a4858a88cd6a4c13d724a~mv2_d_3264_1836_s_2.jpg/v1/fill/w_1417,h_797,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/59faf3_4e0ce7f6660a4858a88cd6a4c13d724a~mv2_d_3264_1836_s_2.jpg" />,
+                  <Image width={500} height={500} src="https://static.wixstatic.com/media/59faf3_f4f24ef0a37b43f5908344ade62625d9~mv2_d_3264_1836_s_2.jpg/v1/fill/w_1417,h_797,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/59faf3_f4f24ef0a37b43f5908344ade62625d9~mv2_d_3264_1836_s_2.jpg" />,
+                ]}
+              />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      </article>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
+    </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        formats: ['image/webp'],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'static.wixstatic.com',
+            port: '',
+            pathname: '/media/**',
+          },
+        ],
+      },
+    
+}
 
 module.exports = nextConfig

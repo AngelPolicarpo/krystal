1b72cea1b59e44094d6e9b96f347cc86cf2bf098
Angel G. Policarpo <102557147+AngelPolicarpo@users.noreply.github.com>
2023-11-14 15:38:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/webp'],
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'static.wixstatic.com',
-            port: '',
-            pathname: '/media/**',
-          },
-        ],
-      },
+    
 }
 
 module.exports = nextConfig
